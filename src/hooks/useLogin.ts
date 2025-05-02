40bc7133070f16e1147d7225acfc777b71b246ce
Sam <quocdoansam.me@gmail.com>
2025-05-02 14:14:39 +0700

    feat: implement useLogin hook for authentication with Axios

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,34 @@
+import { useAuth } from "@/contexts/auth-context";
+import axios from "axios";
+import { useState } from "react";
+
+const useLogin = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const { refetchUser } = useAuth();
+
+  const login = async (id: string, passsword: string) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      await axios.post(
+        "http://localhost:9000/api/auth/token",
+        { id, passsword },
+        { withCredentials: true }
+      );
+      await refetchUser();
+    } catch (err: any) {
+      if (axios.isAxiosError(err)) {
+        setError(err.response?.data?.message || "Login failed.");
+      } else {
+        setError("Unknown Error");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return { login, isLoading, error };
+};
+
+export default useLogin;
